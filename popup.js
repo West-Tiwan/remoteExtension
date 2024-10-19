3d4256286576ca2e0f4e18f01442e78765c997ec
West-Tiwan <xyzmohit.singh@gmail.com>
2024-10-19 16:35:44 +0530

    working, needs some patching

--- popup.js
+++ popup.js
@@ -1,9 +1,28 @@
 const terminal = new Terminal();
 import { IP_ADD } from './CONST.js';
 
+terminal.write('Welcome to the remote terminal!\r\n');
+
 terminal.open(document.getElementById('terminal'));
 
 const ws = new WebSocket(`ws://${IP_ADD}:8080`);
+
+ws.onopen = function () {
+    terminal.write('Connected to the server...\r\n');
+};
+
+ws.onopen = function () {
+    console.log("WebSocket connection established");
+};
+
+ws.onerror = function (error) {
+    console.error("WebSocket error: ", error);
+};
+
+ws.onclose = function () {
+    console.log("WebSocket connection closed");
+};
+
 
 ws.onmessage = function (event) {
     terminal.write(event.data);
